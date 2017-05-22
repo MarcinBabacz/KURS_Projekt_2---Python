068e3c82fe7f6dacf92f7527c34645a91d8f8d43
Marcin <babacz.marcin@gmail.com>
2017-05-22 17:21:06 +0200

    Logowanie v1

--- Weryfikator.py
+++ Weryfikator.py
@@ -8,12 +8,17 @@ has='Lerolg_8183'
 
 class Db():
     def __init__(self):     
+        print('sdgsdgsdgdgdsf')
         self.conn = pymysql.connect('localhost', "root",has,'projekt1')
         sql='SELECT VERSION()'
         self.sql_test(sql)
         
     def sql_test(self,sql):
         try:
+            print('TEST')
+            self.conn = pymysql.connect('localhost', "root",has,'projekt1')
+            
+            print(sql)
             self.c=self.conn.cursor()
             self.c.execute(sql) 
             self.conn.commit()
@@ -21,6 +26,9 @@ class Db():
             print('Błąd połączenia z bazą dancyh')
     
     def odczyt(self,sql):
+        print('odczyt')
+        print(sql)
+        
         
         self.sql_test(sql)
         result=self.c.fetchall()        
@@ -34,18 +42,28 @@ class Db():
 
 
 
-class Logowanie():
+class Logowanie(Db):
     def __init__(self):
         self.menu()
 
     def menu(self):
         choise=''
+        #menu_list=[]
+        
+        
+        
         while (choise!='1' and choise!='2'):
+            
+            
+            
+            
+            
             print(' __________________________')
             print('|        MAIN MENU         |')
             print('|==========================|')
             print('| 1 - Logowanie:           |')
-            print('| 2 - Wyjsćie              |')
+            print('| 2 - Ustawienia           |')
+            print('| 3 - Wyjsćie              |')
             print('|==========================|')
             print('| Podaj numer opcji:       |')
             
@@ -54,7 +72,7 @@ class Logowanie():
         
         if (choise=='1') :
             self.log_input()
-        elif(choise=='2'):
+        elif(choise=='3'):
             print('Wyjście')
     
     def log_input(self):
@@ -63,7 +81,7 @@ class Logowanie():
         print('|==========================|')        
              
         
-        self.usser_id_in=input('Podaj swoje ID: ' )
+        self.usser_id_in=input('| Podaj swoje ID: ' )
      
         sql=("select * from pracownicy where id_pracownika="+self.usser_id_in)
         self.sql_list=mysql.odczyt(sql) 
@@ -75,21 +93,58 @@ class Logowanie():
         else:
             print('Brak  id w bazie')
             self.__init__()    
-    
-    
-    
-    
+ 
     def log_test(self):
+    
+    
+    
+         
+        if(str(self.sql_list[5].strip())==str(self.usser_pass_in.strip())):
+            self.id_roli=(self.sql_list[4])
+            
+            Uzytkownicy(self)
         
-      
-        if(str(self.sql_list[5])==str(self.usser_pass_in)):
-            print('Logowanie OK')
         else:
             print('Błędne hasło')
+            self.menu()
+
         
-        print(str(self.sql_list[5]))
-        print(str(self.usser_pass_in))
+class Uzytkownicy(Logowanie):
+
+    def __init__(self,id_roli):
+
+        self.lista=login.sql_list
+        self.id_roli=id_roli
+        self.role_select()
+    
         
+        
+    def role_select (self):
+        print(' __________________________')
+        print(' __________________________')
+        print('|        MENU'+'%10S'% (self.lista[5])+'|')
+        print('|==========================|')
+        print('| 1 - Logowanie:           |')
+        print('| 2 - Ustawienia           |')
+        print('| 3 - Wyjsćie              |')
+        print('|==========================|')
+        print('| Podaj numer opcji:       |')        
+        
+        if (self.id_roli==1):       
+            print('admin')
+        elif (self.id_roli==2):
+            print('Regionalny')
+        elif (self.id_roli==3):
+            print('Kierownik')       
+        elif (self.id_roli==4):
+            print('Zastępca') 
+        elif (self.id_roli==5):
+            print('Sprzedawca') 
+        elif (self.id_roli==6):
+            print('Gość')  
+        
+
+    
 
 
 
